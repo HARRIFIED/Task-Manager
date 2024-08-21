8d90248c98bc89ab3444ffd59b119baf4dcf6957
HARRIFIED <84249407+HARRIFIED@users.noreply.github.com>
2024-08-21 09:09:29 +0100

    Update conifg

--- backend/config.py
+++ backend/config.py
@@ -6,4 +6,5 @@ class Config:
     basedir = os.path.abspath(os.path.dirname(__file__))
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'db.sqlite')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = "dmfkjkejfelbveibeb"
+   # Add a secret key
+   # SECRET_KEY = ""
